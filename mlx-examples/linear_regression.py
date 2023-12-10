525f1c777c568934f366fc2494e17db5819d0ce6
Ryuichiro Hataya <moskomule@users.noreply.github.com>
2023-12-10 11:44:45 +0900

    initial commit

---
+++ mlx-examples/linear_regression.py
@@ -0,0 +1,63 @@
+# based on https://github.com/ml-explore/mlx/blob/main/examples/python/linear_regression.py
+# Copyright © 2023 Apple Inc.
+
+import time
+
+import mlx.core as mx
+
+
+def main(device, eval):
+    mx.default_stream(getattr(mx, device))
+
+    num_features = 1000
+    num_examples = 1_000
+    num_iters = 10_000
+    lr = 0.01
+    key = mx.random.key(0)
+
+    # True parameters
+    key, key0 = mx.random.split(key)
+    w_star = mx.random.normal((num_features,), key=key0)
+
+    # Input examples (design matrix)
+    key, key0 = mx.random.split(key)
+    X = mx.random.normal((num_examples, num_features), key=key0)
+
+    # Noisy labels
+    key, key0 = mx.random.split(key)
+    eps = 1e-2 * mx.random.normal((num_examples,), key=key0)
+    y = X @ w_star + eps
+
+    # Initialize random parameters
+    key, key0 = mx.random.split(key)
+    w = 1e-2 * mx.random.normal((num_features,), key=key0)
+
+    def loss_fn(w):
+        return 0.5 * mx.mean(mx.square(X @ w - y))
+
+    grad_fn = mx.value_and_grad(loss_fn)
+
+    tic = time.perf_counter()
+    for _ in range(num_iters):
+        loss, grad = grad_fn(w)
+        w = w - lr * grad
+        # this evaluation drastically degenerates the performance even though the final results are identical
+        if eval: mx.eval(w)
+    toc = time.perf_counter()
+
+    _tic = time.perf_counter()
+    loss = loss_fn(w)
+    error_norm = mx.sum(mx.square(w - w_star)).item() ** 0.5
+    throughput = num_iters / (toc - tic)
+    
+    print(f"Loss {loss.item():.5f}, |w-w*| = {error_norm:.5f}, " f"Throughput {throughput:.5f} (it/s)")
+    _toc = time.perf_counter()
+    # print(f"{toc-tic:.5f}s / {_toc-_tic:.5f}s")
+
+if __name__ == "__main__":
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument("--device", choices=("cpu", "gpu"), default="cpu")
+    p.add_argument("--eval", action="store_true")
+    args = p.parse_args()
+    main(args.device, args.eval)
---
+++ mlx-examples/mlp.py
@@ -0,0 +1,88 @@
+# based on https://github.com/ml-explore/mlx-examples/blob/main/mnist/main.py
+# Copyright © 2023 Apple Inc.
+
+import argparse
+import time
+
+
+import mlx.core as mx
+import mlx.nn as nn
+import mlx.optimizers as optim
+
+import mnist
+
+
+def permutation(size: int, key: mx.array=None) -> mx.array:
+    noise = mx.random.uniform(shape=(size,), key=key)
+    return mx.argsort(noise)
+
+class MLP(nn.Module):
+    """A simple MLP."""
+
+    def __init__(self, num_layers: int, input_dim: int, hidden_dim: int, output_dim: int):
+        super().__init__()
+        layer_sizes = [input_dim] + [hidden_dim] * num_layers + [output_dim]
+        self.layers = [nn.Linear(idim, odim) for idim, odim in zip(layer_sizes[:-1], layer_sizes[1:])]
+
+    def __call__(self, x):
+        for l in self.layers[:-1]:
+            x = mx.maximum(l(x), 0.0)
+        return self.layers[-1](x)
+
+
+def loss_fn(model, X, y):
+    return mx.mean(nn.losses.cross_entropy(model(X), y))
+
+
+def eval_fn(model, X, y):
+    return mx.mean(mx.argmax(model(X), axis=1) == y)
+
+
+def batch_iterate(batch_size, X, y, key):
+    perm = mx.array(permutation(y.size, key=key))
+    for s in range(0, y.size, batch_size):
+        ids = perm[s : s + batch_size]
+        yield X[ids], y[ids]
+
+
+def main():
+    
+    num_layers = 2
+    hidden_dim = 32
+    num_classes = 10
+    batch_size = 256
+    num_epochs = 10
+    learning_rate = 1e-1
+
+    key = mx.random.key(0)
+
+    # Load the data
+    train_images, train_labels, test_images, test_labels = map(mx.array, mnist.mnist())
+
+    # Load the model
+    model = MLP(num_layers, train_images.shape[-1], hidden_dim, num_classes)
+    mx.eval(model.parameters())
+
+    loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+    optimizer = optim.SGD(learning_rate=learning_rate)
+
+    for e in range(num_epochs):
+        tic = time.perf_counter()
+        key, key0 = mx.random.split(key)
+        for X, y in batch_iterate(batch_size, train_images, train_labels, key=key0):
+            loss, grads = loss_and_grad_fn(model, X, y)
+            optimizer.update(model, grads)
+            # I don't know this eval is necessary...?
+            # mx.eval(model.parameters(), optimizer.state)
+        accuracy = eval_fn(model, test_images, test_labels)
+        toc = time.perf_counter()
+        print(f"Epoch {e}: Test accuracy {accuracy.item():.3f}," f" Time {toc - tic:.3f} (s)")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Train a simple MLP on MNIST with MLX.")
+    parser.add_argument("--gpu", action="store_true", help="Use the Metal back-end.")
+    args = parser.parse_args()
+    if not args.gpu:
+        mx.set_default_device(mx.cpu)
+    main()
